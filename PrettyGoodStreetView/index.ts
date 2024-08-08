3c8280c3e3879369008d092a6ac7ea0e07a91ba0
Mac Mini <macmini@Macs-Mac-mini.local>
2024-08-09 07:54:15 +1200

    Prepare for GitHub Pages deployment

---
+++ PrettyGoodStreetView/index.ts
@@ -0,0 +1,58 @@
+/**
+ * @license
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+let panorama: google.maps.StreetViewPanorama;
+
+// Function to get URL parameters
+function getUrlParams() {
+  const params = new URLSearchParams(window.location.search);
+  const urllat = params.get('urllat');
+  const urllng = params.get('urllng');
+  const urlhead = params.get('urlhead');
+
+  return { 
+    urllat: urllat ? parseFloat(urllat) : null, 
+    urllng: urllng ? parseFloat(urllng) : null,
+    urlhead: urlhead ? parseFloat(urlhead) : null     
+  };
+}
+
+// Get the parameters
+let { urllat, urllng, urlhead } = getUrlParams();
+
+// Default coordinates if URL parameters are not provided
+// urllat=-36.8645111&urllng=174.7459659 
+const defaultLat = -36.8645111;
+const defaultLng = 174.7459659;
+const defaulthead = 0;
+
+// Example usage: Print the coordinates
+console.log(`Latitude: ${urllat ?? defaultLat}, Longitude: ${urllng ?? defaultLng}, Heading: ${urlhead ?? defaulthead}`);
+
+function initialize() {
+  panorama = new google.maps.StreetViewPanorama(
+    document.getElementById("street-view") as HTMLElement,
+    {
+      position: { 
+        lat: urllat ?? defaultLat, 
+        lng: urllng ?? defaultLng 
+      },
+      pov: {
+        heading: urlhead ?? defaulthead,
+        pitch: 0 
+       },
+      zoom: 1,
+    }
+  );
+}
+
+declare global {
+  interface Window {
+    initialize: () => void;
+  }
+}
+window.initialize = initialize;
+export {};
